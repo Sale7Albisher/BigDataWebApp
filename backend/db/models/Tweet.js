98f3e559fdd8858c1ed33491dfe529da2b51caf2
Saleh Albisher <151204193+Sale7Albisher@users.noreply.github.com>
2025-01-04 16:19:19 +0200

    Update Tweet.js

--- backend/db/models/Tweet.js
+++ backend/db/models/Tweet.js
@@ -2,11 +2,8 @@ const mongoose = require("mongoose");
 
 const tweetSchema = new mongoose.Schema(
   {
-<<<<<<< HEAD
+
     id: { type: String, unique: true, required: true },
-=======
-    unique_tweet_id: { type: String, unique: true, required: true },
->>>>>>> c78c26acd02abddf32a6e551520ae955d40ddeb4
     text: { type: String, required: true },
     username: { type: String, required: true },
     timestamp: { type: String, required: true },
@@ -23,15 +20,7 @@ const tweetSchema = new mongoose.Schema(
   { collection: "stream_data" }
 );
 
-<<<<<<< HEAD
 
-=======
-tweetSchema.index({ text: "text" });
-tweetSchema.index({ sentiment: 1 });
-tweetSchema.index({ "user.screen_name": 1 });
-tweetSchema.index({ created_at: 1 });
-tweetSchema.index({ hashtags: 1 });
-tweetSchema.index({ unique_tweet_id: 1 }, { unique: true });
->>>>>>> c78c26acd02abddf32a6e551520ae955d40ddeb4
+
 
 module.exports = mongoose.model("Tweet", tweetSchema);
